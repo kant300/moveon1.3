e961b705cb2eb363cc25a1ee112a8e1122ae4a47
IceP0tato <rlawodud0313@gmail.com>
2025-11-17 17:56:34 +0900

    251117_관공서 데이터 관련 메소드 추가

--- src/main/java/web/service/living/GovService.java
+++ src/main/java/web/service/living/GovService.java
@@ -49,4 +49,44 @@ public class GovService {
             throw new RuntimeException(e);
         }
     }
+
+    // 분류 코드에 따라 데이터 구하기
+    public List<Map<String, Object>> getData(int code) {
+        List<Map<String, Object>> data = new ArrayList<>();
+        try {
+            // 데이터 파일 경로
+            String path = "src/main/resources/static/data/인천광역시_시설 정보 현황_2021-01-25.csv";
+            FileReader fileReader = new FileReader(path, Charset.forName("EUC-KR"));
+            CSVReader csvReader = new CSVReader(fileReader);
+            List<String[]> csvData = csvReader.readAll();
+
+            for (int i=1; i<csvData.size(); i++) {
+                String[] row = csvData.get(i);
+                Map<String, Object> item = new LinkedHashMap<>();
+
+                // 확인용 코드
+                // System.out.printf("분야 : %s, 유형 : %s, 필드코드 : %s, 타입코드 : %s, 분류코드 : %s,
+                //         시설명: %s, 주소 : %s, 전화번호 : %s, 경도 : %s, 위도 : %s\n",
+                //         row[1], row[2], row[3], row[4], row[5],
+                //         row[8], row[11], row[12], row[13], row[14]);
+
+                // 분류코드에 따라 데이터 추출
+                // ex) 주민자치센터: 1003, 경찰서: 1005, 소방서: 4001
+                if (Integer.parseInt(row[5]) == code) {
+                    // 얻은 데이터로 리스트 만들기
+                    item.put("유형", row[2]);
+                    item.put("시설명", row[8]);
+                    item.put("주소", row[11]);
+                    item.put("전화번호", row[12]);
+                    item.put("경도", row[13]);
+                    item.put("위도", row[14]);
+                    data.add(item);
+                }
+            }
+            csvReader.close();
+            return data;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
