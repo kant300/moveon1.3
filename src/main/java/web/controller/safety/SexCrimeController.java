d6d1fbfe163c6bbb5b5264deeb7f13a04cc4d53a
kant300 <p.o.i.u.0987552@gmail.com>
2025-11-19 15:01:55 +0900

    성범죄 백엔드

---
+++ src/main/java/web/controller/safety/SexCrimeController.java
@@ -0,0 +1,20 @@
+package web.controller.safety;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import web.service.safety.SexCrimeService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/sexcrime")
+public class SexCrimeController {
+
+    private final SexCrimeService sexCrimeService;
+    @GetMapping("/all")
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.ok(sexCrimeService.getAllSexCrime());
+    }
+}
