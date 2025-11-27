17086672f521b1cc6552fcb9d8f6389a1554ebe0
jihoons1 <gnsthd0621@naver.com>
2025-11-27 16:02:11 +0900

    guest post > put 변경

--- src/main/java/web/controller/WeatherController.java
+++ src/main/java/web/controller/WeatherController.java
@@ -1,15 +1,13 @@
 package web.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import web.service.WeatherService;
 
 @RestController
 @RequestMapping("/weather")
 @RequiredArgsConstructor
+@CrossOrigin( origins = "*")
 public class WeatherController {
     private final WeatherService weatherService;
 
